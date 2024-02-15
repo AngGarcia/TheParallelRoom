d134827d6c5c177496aa259c4b8fd6299f9712b0
Pablo Carrasco Velo <119067966+Exileplayerr@users.noreply.github.com>
2024-02-15 19:03:17 +0100

    MainMenu change scene
    You can now collide with the poster or grab it in order to change scenes.

---
+++ Assets/Pablo/Poster.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Poster : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    SceneChanger sceneChanger;
+
+    private void Start()
+    {
+        
+        sceneChanger = new SceneChanger();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Entro");
+            sceneChanger.lvl1();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Entro");
+            sceneChanger.lvl1();
+        }
+    }
+
+    public void goToLvl1()
+    {
+        sceneChanger.lvl1();
+    }
+}
---
+++ Assets/Pablo/SceneChanger.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //public AudioSource audioSource;
+    //public AudioClip button;
+    private void Start()
+    {
+        Time.timeScale = 1;
+    }
+    public void lvl1()
+    {
+        //StartCoroutine(delayLvl1());
+        SceneManager.LoadScene("Lvl1_Mines");
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void mainMenu()
+    {
+        //StartCoroutine (delayMainMenuScreen());
+        //MusicManager.Instance.PlayBackgroundMusic(AppSounds.MAIN_MENU_MUSIC);
+        SceneManager.LoadScene("MainMenu");
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void exitGame()
+    {
+        //StartCoroutine(delayExit());
+        //MusicManager.Instance.StopBackgroundMusic();
+        Application.Quit();
+
+    }
+
+    IEnumerator delayLvl1()
+    {
+        //audioSource.PlayOneShot(button);
+        //yield return new WaitForSecondsRealtime(button.length);
+        yield return new WaitForSeconds(2.0f);
+        SceneManager.LoadScene("Lvl1_Mines");
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    IEnumerator delayMainMenuScreen()
+    {
+        //audioSource.PlayOneShot(button);
+        //yield return new WaitForSecondsRealtime(button.length);
+        yield return new WaitForSeconds(2.0f);
+        SceneManager.LoadScene("MainMenu");
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    IEnumerator delayExit()
+    {
+        //audioSource.PlayOneShot(button);
+        //yield return new WaitForSeconds(button.length);
+        yield return new WaitForSeconds(2.0f);
+        Application.Quit();
+    }
+}
