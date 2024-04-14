90b0948b735b04ce90b54f20610edfeda04cde0c
Pablo Carrasco Velo <119067966+Exileplayerr@users.noreply.github.com>
2024-04-15 01:30:03 +0200

    ChessPuzzle

---
+++ Assets/SocketControl.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class SocketControl : MonoBehaviour
+{
+    public XRSocketInteractor socket;
+    public chessPuzzle puzzle;
+    public int squareType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        socket = GetComponent<XRSocketInteractor>();
+    }
+
+    public void SocketCheck()
+    {
+        GameObject item = socket.selectTarget.gameObject;
+        if (item.CompareTag("Pawn") && squareType==0)
+        {
+            //Debug.Log("ENTRO");
+            //item.GetComponent<XRGrabInteractable>().
+            item.GetComponent<Rigidbody>().isKinematic = true;
+            socket.socketActive = false;
+            
+            StartCoroutine(resetSocket(item));
+            //socket.socketActive = true;
+        }
+        else if(item.CompareTag("Pawn") && squareType==1)
+        {
+            puzzle.winPuzzle();
+        }
+    }
+
+    IEnumerator resetSocket(GameObject item)
+    {
+        yield return new WaitForSeconds(1);
+        puzzle.losePuzzle();
+        item.GetComponent<Rigidbody>().isKinematic = false;
+        socket.socketActive = true;
+    }
+
+}
---
+++ Assets/chessPuzzle.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class chessPuzzle : MonoBehaviour
+{
+    public GameObject pawn;
+    private Transform respawnPosition;
+    public Vector3 pawnPosition;
+    //public float addToRes = -0.1f;
+
+    //0=NOTHING
+    //1=LOSE
+    //2=WIN
+    public bool puzzlePassed = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        respawnPosition = pawn.transform;
+        pawnPosition = respawnPosition.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private IEnumerator resetPawnPosition()
+    {
+        puzzlePassed = false;
+        yield return new WaitForSeconds(0.25f);
+        //pawn.transform.position = new Vector3(pawn.transform.position.x + addToRes, pawn.transform.position.y, pawn.transform.position.z);
+        pawn.transform.position = pawnPosition;
+    }
+
+    public void losePuzzle()
+    {
+
+        StartCoroutine(resetPawnPosition());
+    }
+
+    public void winPuzzle()
+    {
+        puzzlePassed = true;
+        pawn.GetComponent<XRGrabInteractable>().enabled = false;
+        //Deshabilitar sockets?
+    }
+
+}
