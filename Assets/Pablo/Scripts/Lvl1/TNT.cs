eb416af5283703c4f173ce31dce4819dce973e7c
Pablo Carrasco Velo <119067966+Exileplayerr@users.noreply.github.com>
2024-04-23 19:20:56 +0200

    TNT now kills you

---
+++ Assets/Pablo/Scripts/Lvl1/TNT.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TNT : MonoBehaviour
+{
+    public GameObject flame;
+    public GameObject explosion;
+    public float timeToExplode;
+    public float timeToRespawn;
+    public float distanceToDie = 1.0f;
+    private GameObject player;
+    private float distance;
+    public Transform respawnPosition;
+    public GameObject dinamite;
+    public GameObject parent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "BlowtorchFlame")
+        {
+            flame.SetActive(true);
+            StartCoroutine(explosionCountdown(timeToExplode));
+        }
+
+        if (other.gameObject.tag == "Match")
+        {
+            flame.SetActive(true);
+            StartCoroutine(explosionCountdown(timeToExplode));
+        }
+    }
+
+    IEnumerator explosionCountdown(float timeToExplode) 
+    {
+        yield return new WaitForSeconds(timeToExplode);
+        flame.SetActive(false);
+        explosion.SetActive(true);
+        StartCoroutine(explode(1.3f));
+        
+    }
+
+    IEnumerator explode(float explosionTime)
+    {
+        yield return new WaitForSeconds(explosionTime);
+        explosion.SetActive(false);
+        distance = Mathf.Abs(Vector3.Distance(player.transform.position, this.transform.position));
+        if (distance < distanceToDie)
+        {
+            GameManager.SceneChangerInstance.mainMenu();
+        }
+        else
+        {
+            StartCoroutine(respawnTNT(timeToRespawn));
+        }
+    }
+
+    IEnumerator respawnTNT(float timeToRespawn)
+    {
+        yield return new WaitForSeconds(timeToRespawn);
+        GameObject din = Instantiate(dinamite);
+        din.transform.position = respawnPosition.position;
+        Destroy(parent);
+    }
+}
