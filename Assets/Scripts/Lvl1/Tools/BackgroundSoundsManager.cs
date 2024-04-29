b97c90da96942f5c100496617ed1d493fa55bd71
Elisa Alonso <elisaalonsosa@gmail.com>
2024-04-29 18:14:56 +0200

    Lvl1 - Added background sfx and music

--- Assets/Scripts/Lvl1/Tools/BackgroundSoundsManager.cs
+++ Assets/Scripts/Lvl1/Tools/BackgroundSoundsManager.cs
@@ -7,23 +7,59 @@ public class BackgroundSoundsManager : MonoBehaviour
     [SerializeField]
     private AudioClip voices;
     [SerializeField]
-    private AudioClip mineroSong;
+    private AudioClip rocksFalling;
+    [SerializeField]
+    private AudioClip explosion;
 
-    private AudioSource audioSource;
-    private float delayTime = 20f;
+    [SerializeField]
+    private AudioClip mineroSong;
+    [SerializeField]
+    private AudioClip ambientMusic;
+
+    [SerializeField]
+    private AudioSource audioSourceSFX;
+    [SerializeField]
+    private AudioSource audioSourceMusic;
+    private float delayTime = 30f;
 
     void Start()
     {
-        audioSource = this.gameObject.GetComponent<AudioSource>();
-        StartCoroutine(PlayAudioWithDelay());
+        //StartCoroutine(PlayAudioWithDelay());
+        audioSourceMusic.clip = ambientMusic;
+        audioSourceMusic.Play();
+        StartCoroutine(PlayMineSFXWithDelay());
     }
 
-    IEnumerator PlayAudioWithDelay()
+    IEnumerator PlayVoicesWithDelay()
     {
         while (true)
         {
-            audioSource.PlayOneShot(voices);
+            audioSourceSFX.PlayOneShot(voices);
             yield return new WaitForSeconds(voices.length + delayTime);
         }
     }
+
+    IEnumerator PlayMineSFXWithDelay()
+    {
+
+        float num = 0.0f;
+
+        while (true)
+        {
+
+            // Generar un número aleatorio entre 0 y 2
+            num = Mathf.RoundToInt(Random.Range(0f, 1f));
+
+            if(num == 0)
+            {
+                audioSourceSFX.PlayOneShot(rocksFalling);
+            }
+            else
+            {
+                audioSourceSFX.PlayOneShot(explosion);
+            }
+            yield return new WaitForSeconds(delayTime);
+        }
+    }
+
 }
