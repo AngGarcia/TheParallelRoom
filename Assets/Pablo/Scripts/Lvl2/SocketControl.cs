ca3d8bf4cbcb646a957d9c7ae28d28d6ca660404
Exileplayerr <pcv2.pcv@gmail.com>
2024-05-11 20:11:05 +0200

    ChessPuzzle Update

--- Assets/Pablo/Scripts/Lvl2/SocketControl.cs
+++ Assets/Pablo/Scripts/Lvl2/SocketControl.cs
@@ -8,6 +8,7 @@ public class SocketControl : MonoBehaviour
     private XRSocketInteractor socket;
     public chessPuzzle puzzle;
     public int squareType;
+    public int squareFilter;
 
     // Start is called before the first frame update
     void Start()
@@ -18,26 +19,67 @@ public class SocketControl : MonoBehaviour
     public void SocketCheck()
     {
         GameObject item = socket.selectTarget.gameObject;
-        if (item.CompareTag("Pawn") && squareType==0)
+        if (item.CompareTag("Pawn") && squareType==0 && squareFilter==0)
         {
             //Debug.Log("ENTRO");
             //item.GetComponent<XRGrabInteractable>().
             item.GetComponent<Rigidbody>().isKinematic = true;
             socket.socketActive = false;
             
-            StartCoroutine(resetSocket(item));
+            StartCoroutine(resetSocket(item, 0));
             //socket.socketActive = true;
         }
-        else if(item.CompareTag("Pawn") && squareType==1)
+        else if(item.CompareTag("Pawn") && squareType==1 &&squareFilter==0)
         {
             puzzle.winPuzzle();
         }
+        else if (item.CompareTag("Knight") && squareType == 0 && squareFilter == 1)
+        {
+            //Debug.Log("ENTRO");
+            //item.GetComponent<XRGrabInteractable>().
+            item.GetComponent<Rigidbody>().isKinematic = true;
+            socket.socketActive = false;
+
+            StartCoroutine(resetSocket(item, 1));
+            //socket.socketActive = true;
+        }
+        else if (item.CompareTag("Bishop1") && squareType == 0 && squareFilter == 2)
+        {
+            //Debug.Log("ENTRO");
+            //item.GetComponent<XRGrabInteractable>().
+            item.GetComponent<Rigidbody>().isKinematic = true;
+            socket.socketActive = false;
+
+            StartCoroutine(resetSocket(item, 2));
+            //socket.socketActive = true;
+        }
+        else if (item.CompareTag("Bishop2") && squareType == 0 && squareFilter == 3)
+        {
+            //Debug.Log("ENTRO");
+            //item.GetComponent<XRGrabInteractable>().
+            item.GetComponent<Rigidbody>().isKinematic = true;
+            socket.socketActive = false;
+
+            StartCoroutine(resetSocket(item, 3));
+            //socket.socketActive = true;
+        }
+        else if (item.CompareTag("King") && squareType == 0 && squareFilter == 4)
+        {
+            //Debug.Log("ENTRO");
+            //item.GetComponent<XRGrabInteractable>().
+            item.GetComponent<Rigidbody>().isKinematic = true;
+            socket.socketActive = false;
+
+            StartCoroutine(resetSocket(item, 4));
+            //socket.socketActive = true;
+        }
+
     }
 
-    IEnumerator resetSocket(GameObject item)
+    IEnumerator resetSocket(GameObject item, int resetType)
     {
         yield return new WaitForSeconds(0.5f);
-        puzzle.losePuzzle();
+        puzzle.losePuzzle(resetType);
         item.GetComponent<Rigidbody>().isKinematic = false;
         socket.socketActive = true;
     }
